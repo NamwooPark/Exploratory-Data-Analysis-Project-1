3fcd8279225c1a9af71129258b8fc9815db0e9ec
woodypark <np272@georgetown.edu>
2016-08-27 16:22:25 -0400

    Delete plot2.R

--- plot2.R
+++
@@ -1,9 +0,0 @@
-# transforming dataset
-subdata <- subdata %>%
-      mutate(time = ymd_hms(paste(Date, Time)))
-
-# plotting and creating png file
-with(subdata, plot(time, Global_active_power, type="l", xlab="", 
-      ylab="Global Active Power (killowatts)"))
-dev.copy(png, file="plot2.png", height=600, width=700)
-dev.off()
